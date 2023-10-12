09ab2ea36c52132aaaa5f0a53e9a1ae918d57fbe
Akash <akashbytes@gmail.com>
2023-10-12 18:00:22 +0530

    added side and main component to display ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import SideBar from './Components/SideBar';
+import MainCounter from './Components/MainCounter';
 
 function App() {
-  return (
-    <div className="App">
-     <h1>hiiii</h1>
-    </div>
-  );
+  return (<div className="flex border border-red-600 w-[100%] h-[90rem]">
+<SideBar></SideBar>
+<MainCounter></MainCounter>
+  </div>);
 }
 
 export default App;
