7c3949993a79b606827faa2a850f209a31299b0b
Akash <akashbytes@gmail.com>
2023-10-12 22:39:29 +0530

    code modified...

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@
 import './App.css';
 import SideBar from './Components/SideBar';
 import MainCounter from './Components/MainCounter';
+// import {createBrowserRouter} from "react-router-dom"
+
+
+
+// const AppRoutes = createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<MainCounter/>
+//   }
+// ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function App() {
   return (<div className="flex border border-red-600 w-[100%] h-[90rem]">
